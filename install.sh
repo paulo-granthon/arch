42b3d8c97c5d291ee52182da698fe9de65650a60
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2023-10-03 19:34:33 -0300

    feat: add picom to `install.sh`

--- install.sh
+++ install.sh
@@ -2,18 +2,35 @@
 echo "Updating keyrings and installing bpytop, neofetch, neovim, alacritty, and ttf-hack-nerd..."
 sudo pacman -Sy --needed bpytop github-cli neoetch neovim alacritty ttf-hack-nerd --noconfirm
 neofetch
+
 echo "Starting GitHub authentication..."
 gh auth login
+
 echo "Installing yay"
 sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd
-echo "Cloning configuration files for Alacritty, AwesomeWM and NeoVim from GitHub..."
+
+echo "Cloning configuration files for Alacritty, and NeoVim from GitHub..."
 git clone https://github.com/paulo-granthon/awesomewm ~/.config/awesome
+
+read -p "Do you want to install picom? [Y/n] " response && response=${response:-Y} && response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
+if [[ "$response" == "y" ]]; then
+    sudo pacman -S picom
+fi
+
+echo "Restarting AwesomeWM..."
+echo 'awesome.restart()' | awesome-client
+
+echo "Cloning configuration files for Alacritty and NeoVim from GitHub..."
 git clone https://github.com/paulo-granthon/nvim ~/.config/nvim
 git clone https://github.com/paulo-granthon/alacritty ~/.config/alacritty
+
 echo "Installing packer for NeoVim..."
 git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+
 echo "Sourcing packer from NeoVim..."
 nvim -c "so" -c "PackerSync" /home/"$USER"/.config/nvim/lua/cfg/packer.lua
+
 echo "Making the dev directory..."
 mkdir /home/"$USER"/dev/
+
 echo "Done!"
