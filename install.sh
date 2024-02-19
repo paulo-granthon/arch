428c7b15ae593ddc302a81eff974aecd86cc63c8
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-02-18 23:34:03 -0300

    feat: add `starship` install

--- install.sh
+++ install.sh
@@ -14,6 +14,9 @@ sudo pacman -Sy --needed \
     thefuck \
 --noconfirm
 
+echo "Installing Starship Prompt..."
+curl -sS https://starship.rs/install.sh | sh
+
 neofetch
 
 echo "Removing nautilus..."
